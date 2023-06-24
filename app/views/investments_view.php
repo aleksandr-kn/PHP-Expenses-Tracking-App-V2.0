5f87bf96358d74a43cf67ee9d8ddb1e9a8004c2f
aleksandr-kn <a.kanygin99@gmail.com>
2023-06-25 00:43:03 +0300

    Убрал лишний элемент

--- app/views/investments_view.php
+++ app/views/investments_view.php
@@ -6,9 +6,6 @@
         <div class="content-wrapper">
             <div class="page-header">
                 <h3 class="page-title">
-                    <span class="page-title-icon bg-gradient-primary text-white mr-2">
-                        <i class="mdi mdi-home"></i>
-                    </span>
                     Управление инвестициями
                 </h3>
                 <nav aria-label="breadcrumb">
